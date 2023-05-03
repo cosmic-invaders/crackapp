1e31fd0de70096b6f30c15fb03ea0ba2f7e4ebbb
jaswanthreddy-007 <88081034+jaswanthreddy-007@users.noreply.github.com>
2023-05-04 00:37:59 +0530

    Image pickeer working

--- lib/pages/scan.dart
+++ lib/pages/scan.dart
@@ -1,18 +1,90 @@
+// import 'dart:js_util';
+
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
 
 class scan extends StatefulWidget {
-  const scan({Key? key}) : super(key: key);
+  const scan({Key? key,}) : super(key: key);
+
+
 
   @override
-  State<scan> createState() => _scanState();
-}
+  State<scan> createState() => _scanState();}
 
 class _scanState extends State<scan> {
+
+  File? image;
+
+  Future pickImage() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+
+      if(image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Future pickImageC() async {
+    try {
+      final image = await ImagePicker().pickImage(source: ImageSource.camera);
+
+      if(image == null) return;
+
+      final imageTemp = File(image.path);
+
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch(e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('scan here',style: TextStyle(fontSize: 70),)),
+        appBar: AppBar(
+          title: const Text("Image Picker Example"),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                      "Pick Image from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold
+                      )
+                  ),
+                  onPressed: () {
+                    pickImage();
+                  }
+              ),
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text(
+                      "Pick Image from Camera",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold
+                      )
+                  ),
+                  onPressed: () {
+                    pickImageC();
+                  }
+              ),
+              SizedBox(height: 20,),
+              image != null ? Image.file(image!): Text("No image selected")
+            ],
+          ),
+        )
     );
   }
 }
+
